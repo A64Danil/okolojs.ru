2645ccc4fc2f72c3d29876ebece11eef65d913d2
DanilZen <a64danil@mail.ru>
2019-09-23 21:36:00 +0300

    fix typo in all files in commit

--- src/js/index.js
+++ src/js/index.js
@@ -31,6 +31,7 @@ function getTest() {
     } else {
         // вывести результат
         console.log( xhr.responseText ); // responseText -- текст ответа.
+        return xhr.responseText;
     }
 }
 
@@ -39,44 +40,8 @@ function getTest() {
 var source = document.getElementById("trend-template").innerHTML;;
 var template = Handlebars.compile(source);
 
-var data = {
-    "trends": [
-        {
-            "title": "Изучите Nodejs создав 12 проектов",
-            "titleOriginal": "Learn Nodejs by building 12 projects",
-            "description": "Вы узнаете о серверном JavaScript, модулях Node и NPM, использовании других связанных технологий и инфраструктур при построении двенадцати проектов. Технологии, охватываемые курсом, включают в себя технологии HTML / CSS Frontend, Nodejs NPM, базу данных NoSQL, Column DB, базу данных ORM, Express Framework, асинхронное программирование, Karken layer, управление пользователями Drywall, шифрование Bcrypt, API Socket IO и REST.",
-            "language": "английский",
-            "difficulty": "Средний",
-            "author": "Udemy (Brad Tavares)",
-            "url": "https://coursehunter.net/course/izuchite-nodejs-sozdav-12-proektov",
-            "published": "11.01.2017",
-            "started": "1.04.2019",
-            "status": "Будет завершен 1.11.2019"
-        },
-        {
-            "title": "Базовый курс по JS 2018",
-            "titleOriginal": "",
-            "description": "Свежий курс по комплексному обучению языка Javascript от Loftschool. Если вы хотите сложить по полочкам все свои знания о javascript а также поднять свой скилл на новый уровень - этот курс призван вам в этом помочь.",
-            "language": "русский",
-            "difficulty": "Лёгкий",
-            "author": "LoftSchool",
-            "url": "https://coursehunter.net/course/kompleksnoe-obuchenie-javascript",
-            "published": "18.01.2018",
-            "started": "1.02.2018",
-            "status": "Завершен 1.07.2018"
-        }
-    ]
-};
-
-var result = template(data);
+var result = template(getTest());
 
 TRENDS.innerHTML = result;
 
 console.log(result);
-
-
-data.forEach(){
-    title == .... {
-        return id
-    }
-}
